88e961c3c39aa5550800d5fb439d6bc44655a37f
Stefan Stjepic <stefan.stjepic03@gmail.com>
2026-06-02 08:52:56 +0200

    [ADD] Frontend project setup

--- banknote-grading-system/service/src/main/java/com/sbnz/service/controllers/BanknoteGradingController.java
+++ banknote-grading-system/service/src/main/java/com/sbnz/service/controllers/BanknoteGradingController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/banknote")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class BanknoteGradingController {
 
     private final BanknoteGradingService banknoteGradingService;
